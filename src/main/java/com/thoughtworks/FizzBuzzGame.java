ae67311b2882bd28b94e49558fa154c276146d28
xiefei <xiefei0887@163.com>
2018-04-14 13:05:51 +0800

    implemented function

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -6,10 +6,46 @@ import java.util.List;
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
+    private int[] specialNum = {3, 5, 7};
 
     public void start(int count) {
-
+        for (int i = 1; i <= count; i++) {
+            if (containNum(i, specialNum[0])) {
+                results.add("Fizz");
+                continue;
+            }
+            String str = getOutput(i);
+            if (str.equals(""))
+                results.add(Integer.toString(i));
+            else
+                results.add(str);
+        }
     }
+
+    public String getOutput(int i) {
+        String str = "";
+        if (isMultipliOfNum(i, specialNum[0]))
+            str += "Fizz";
+        if (isMultipliOfNum(i, specialNum[1]))
+            str += "Bizz";
+        if (isMultipliOfNum(i, specialNum[2]))
+            str += "Whizz";
+        return str;
+    }
+
+    public boolean isMultipliOfNum(int integer, int num) {
+        return integer % num == 0;
+    }
+
+    public boolean containNum(int integer, int num) {
+        char[] cs = Integer.toString(integer).toCharArray();
+        for (char c : cs) {
+            if (Character.getNumericValue(c) == num)
+                return true;
+        }
+        return false;
+    }
+
 
     public List<String> getResults() {
         return results;
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,9 +2,12 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
-
+        FizzBuzzGame game = new FizzBuzzGame();
+        assertThat(game.getOutput(3)).isEqualTo("Fizz");
     }
 }
