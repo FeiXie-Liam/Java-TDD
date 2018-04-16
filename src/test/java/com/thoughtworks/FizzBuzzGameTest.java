4bbd8b8925a80fc6a08bbc88902a54613a635805
FeiXie-Liam <xiefei0887@163.com>
2018-04-16 17:34:02 +0800

    finished.

--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -10,4 +10,29 @@ public class FizzBuzzGameTest {
         FizzBuzzGame game = new FizzBuzzGame();
         assertThat(game.getOutput(3)).isEqualTo("Fizz");
     }
+
+    @Test
+    public void should_Return_Fizz_when_is_Multiple_of_3() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        assertThat(game.getOutput(9)).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void should_Return_Bizz_when_is_Multiple_of_5() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        assertThat(game.getOutput(10)).isEqualTo("Bizz");
+    }
+
+    @Test
+    public void should_Return_Whizz_when_is_Multiple_of_7() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        assertThat(game.getOutput(14)).isEqualTo("Whizz");
+    }
+
+    @Test
+    public void should_Return_FizzBizz_when_is_Multiple_of_3_and_5(){
+        FizzBuzzGame game = new FizzBuzzGame();
+        assertThat(game.getOutput(15)).isEqualTo("FizzBizz");
+    }
+
 }
